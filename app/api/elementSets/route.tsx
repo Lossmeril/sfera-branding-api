1bd2b3d7881cb5fa89fbc34ed7102f42de852e1e
Michal Špitálský <michal.spitalsky@pm.me>
2025-10-21 14:15:22 +0200

    fix (api/elementSets): elementSets now returns normalised results

--- app/api/elementSets/route.tsx
+++ app/api/elementSets/route.tsx
@@ -4,6 +4,6 @@ import { expandAllElementSets } from "@/lib/utils";
 
 export async function GET() {
   // If you want to include expanded lists for each set:
-  const final = expandAllElementSets(elementSets);
-  return NextResponse.json({ final });
+
+  return NextResponse.json(expandAllElementSets(elementSets));
 }
